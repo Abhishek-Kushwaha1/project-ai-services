34c0f3ed2e756b73c091663b0595c1cfc5be6e40
A-S-Prajna <a.s.prajna@ibm.com>
2025-12-10 06:51:14 +0000

    Adding Put and delete method
    Signed-off-by: Abhishek-Kushwaha <Abhishek.Kushwaha@ibm.com>

--- ai-services/tests/e2e/httpclient/client.go
+++ ai-services/tests/e2e/httpclient/client.go
@@ -1,47 +1,53 @@
 package httpclient
 
 import (
-    "bytes"
-    "encoding/json"
-    "io"
-    "net/http"
-    "os"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 )
 
-type HttpClient struct {
-    BaseURL string
-    Client  *http.Client
+type HTTPClient struct {
+	BaseURL    string
+	HTTPClient *http.Client
 }
 
-func NewHttpClient() *HttpClient {
-    baseURL := os.Getenv("API_BASE_URL")
-    if baseURL == "" {
-        baseURL = "http://localhost:8080"
-    }
-    return &HttpClient{
-        BaseURL: baseURL,
-        Client:  &http.Client{},
-    }
+func NewHTTPClient() *HTTPClient {
+	baseURL := os.Getenv("AI_SERVICES_BASE_URL")
+	if baseURL == "" {
+		baseURL = "http://localhost:8080"
+	}
+	return &HTTPClient{
+		BaseURL: baseURL,
+		HTTPClient: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
 }
 
-func (c *HttpClient) Get(path string) (*http.Response, error) {
-    req, err := http.NewRequest("GET", c.BaseURL+path, nil)
-    if err != nil {
-        return nil, err
-    }
-    return c.Client.Do(req)
+func (c *HTTPClient) buildURL(path string) string {
+	return fmt.Sprintf("%s%s", c.BaseURL, path)
 }
 
-func (c *HttpClient) Post(path string, body interface{}) (*http.Response, error) {
-    var buf io.Reader
-    if body != nil {
-        b, _ := json.Marshal(body)
-        buf = bytes.NewBuffer(b)
-    }
-    req, err := http.NewRequest("POST", c.BaseURL+path, buf)
-    if err != nil {
-        return nil, err
-    }
-    req.Header.Set("Content-Type", "application/json")
-    return c.Client.Do(req)
+func (c *HTTPClient) Get(path string) (*http.Response, error) {
+	return c.HTTPClient.Get(c.buildURL(path))
+}
+
+func (c *HTTPClient) Post(path string, body interface{}) (*http.Response, error) {
+	b, _ := json.Marshal(body)
+	return c.HTTPClient.Post(c.buildURL(path), "application/json", bytes.NewBuffer(b))
+}
+
+func (c *HTTPClient) Put(path string, body interface{}) (*http.Response, error) {
+	b, _ := json.Marshal(body)
+	req, _ := http.NewRequest("PUT", c.buildURL(path), bytes.NewBuffer(b))
+	req.Header.Set("Content-Type", "application/json")
+	return c.HTTPClient.Do(req)
+}
+
+func (c *HTTPClient) Delete(path string) (*http.Response, error) {
+	req, _ := http.NewRequest("DELETE", c.buildURL(path), nil)
+	return c.HTTPClient.Do(req)
 }
